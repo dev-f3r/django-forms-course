8f9686f9c86df48f4fd15d1bc9842ddf217d71f1
devfer <devfercastro@gmail.com>
2024-02-21 22:39:32 -0300

    Documment

--- pizza/views.py
+++ pizza/views.py
@@ -11,15 +11,19 @@ def order(request):
     if request.method == "POST":
         # Capture all the information of the POST request into a `PizzaForm` instance
         filled_form = PizzaForm(request.POST)
+
+        # If `filled_form` is valid
         if filled_form.is_valid():
+            # Make a message with the form information
             note = "Thanks for ordering! You %s with %s and %s is on it's way" % (
                 filled_form.cleaned_data["size"],
                 filled_form.cleaned_data["topping1"],
                 filled_form.cleaned_data["topping2"],
                 )
-            # note = f"Thanks for ordering! You {filled_form.cleaned_data["size"]} with {filled_form.cleaned_data["topping1"]} and {filled_form.cleaned_data["topping2"]} is on its way"
 
+            # Make a new form
             new_form = PizzaForm()
+            # Render the order template and pass the new form and the message
             return render(request, "pizza/order.html", {"pizzaform": new_form, "note": note})
     # Handling the rest of http methods
     else:
