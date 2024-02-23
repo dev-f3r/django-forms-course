bd6c171eb0f45a4afe7db5691351b87bdaa04d6e
devfer <devfercastro@gmail.com>
2024-02-22 22:00:22 -0300

    Implement PizzaAdmin class for customized pizza display

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Size, Pizza
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ("topping1", "topping2", "size")
+
+admin.site.register(Pizza, PizzaAdmin)
 admin.site.register(Size)
