062b8114fd525c9b6331e4665721c655a88cfeb2
devfer <devfercastro@gmail.com>
2024-02-23 20:53:11 -0300

    New field in admin display pizzas

--- pizza/admin.py
+++ pizza/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Size, Pizza
 
 class PizzaAdmin(admin.ModelAdmin):
-    list_display = ("topping1", "topping2", "size")
+    list_display = ("topping1", "topping2", "size", "pk")
 
 admin.site.register(Pizza, PizzaAdmin)
 admin.site.register(Size)
